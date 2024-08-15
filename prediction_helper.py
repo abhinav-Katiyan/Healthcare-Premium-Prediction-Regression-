ad0fcdc3cd0a259b169cf4276eb33a18ccb07a65
Abhinav <abhinavkatiyan21@gmail.com>
2024-08-15 16:45:15 +0530

    push streamlit app code

--- prediction_helper.py
+++ prediction_helper.py
@@ -4,7 +4,7 @@ import pandas as pd
 model_rest = joblib.load("Artifacts/Plzzz_Work_this_time_rest.joblib")
 model_young = joblib.load("Artifacts/Plzzz_Work_this_time_young.joblib")
 scaler_young = joblib.load("Artifacts/scaler_young_plzzz.joblib")
-scaler_rest = joblib.load("Artifacts/scaler_rest_plzzz.joblib.")
+scaler_rest = joblib.load("Artifacts/scaler_rest_plzzz (1).joblib")
 
 
 def calculate_normalized_risk(medical_history):
